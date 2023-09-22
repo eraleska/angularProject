9463fe299b5f09282d343dfe5246a2cfadaa845b
eraleska <garettool@mail.ru>
2023-09-22 15:23:02 +0300

    initial commit
    angular weather app created

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'e397e69842msha07f158040512dap14e1e9jsn6993d8f11a7a'
+};
